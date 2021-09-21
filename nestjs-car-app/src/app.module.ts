619879ae94186483257b52af4bfa3cf31b681d94
Yuya Furusawa <fullyou0798@gmail.com>
2021-09-22 00:37:18 +0900

    first commit

---
+++ nestjs-car-app/src/app.module.ts
@@ -0,0 +1,26 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { GraphQLModule } from '@nestjs/graphql';
+
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { DatabaseModule } from './database/database.module';
+import { ComponentsModule } from './components/components.module';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot(),
+    DatabaseModule,
+    GraphQLModule.forRoot({
+      // playground: process.env.NODE_ENV === 'development' ? true : false,
+      playground: true,
+      // debug: process.env.NODE_ENV === 'development' ? true : false,
+      debug: true,
+      autoSchemaFile: true,
+    }),
+    ComponentsModule,
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
